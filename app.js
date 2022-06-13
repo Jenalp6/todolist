d2d04c14a64ddd3ecac8a279f2b139b8326de4bb
jenalp6 <jenalp.18@gmail.com>
2022-06-13 23:10:37 +0300

    version1.5

--- app.js
+++ app.js
@@ -1,64 +1,54 @@
-let items = ["item1", "item2", "item3", "item5"];
+//* ======================================================
+//*                     TODO APP
+//* ======================================================
+//? Selectors
+const btn = document.getElementById("todo-button");
+const todoInput = document.getElementById("todo-input");
+const todoUl = document.getElementById("todo-ul");
 
-let list = document.querySelector("#myList");
-
-items.forEach(function (item) {
-  let li = document.createElement("li");
-  let t = document.createTextNode(item);
-  console.log(t);
-  li.className = "list-group-item";
-  li.appendChild(t);
-  list.appendChild(li);
-
-  let span = document.createElement("span");
-  let text = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(text);
-  li.appendChild(span);
-
-  span.onclick = function (e) {
-    let li = this.parentElement;
-    li.style.display = "none";
-    li.classList.remove("checked");
-  };
-});
-
-//? span onclick uzun yol //
-// let close = document.getElementsByClassName("close");
-
-// for (let i = 0; i < close.length; i++) {
-//   close[i].onclick = function () {
-//     let li = this.parentElement;
-//     li.style.display = "none";
-//   };
-// }
-
-list.addEventListener("click", (item) => {
-  if (item.target.tagName == "LI") {
-    item.target.classList.toggle("checked");
-    ToggleDeleteButton();
-  }
-});
-document.querySelector("#deleteAll").onclick = function () {
-  let elements = document.querySelector(".checked");
-  elements.forEach((v) => {
-    v.style.display = "none";
-  });
+//? Baslangicta input aktif olsun
+window.onload = function () {
+  todoInput.focus();
 };
 
-function ToggleDeleteButton() {
-  let checkList = document.querySelectorAll(".list-group-item.checked"); //or .checked
-  if (checkList.length > 0) {
-    document.querySelector("#deleteAll").classList.remove("d-none");
+//? Add Buton Event'inin tanimanmasi
+btn.addEventListener("click", (e) => {
+  if (!todoInput.value) {
+    alert("Please enter your todo");
   } else {
-    document.querySelector("#deleteAll").classList.add("d-none");
+    todoUl.innerHTML += `
+    <li>
+      <i class="fa fa-check"></i>
+      <p>${todoInput.value}</p>
+      <i class="fa fa-trash"></i>
+    </li>`;
+    todoInput.value = "";
   }
-}
+});
 
-document.querySelector("#btnCreate").onclick = function () {
-  let item = document.querySelector("txtItem").value;
-  if (item === "") {
-    alert("Lütfen bir değer giriniz");
-    return;
+//? Klavyeden enter tusuna basilmasi ile add butonunun click fonksiyonunun cagrilmasi
+todoInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    btn.click();
   }
-};
+});
+
+//? delete ve ok butonları için event tanimlamasi
+todoUl.addEventListener("click", (e) => {
+  //!Event, sil butonlarının birisinden geldi ise
+  if (e.target.classList.contains("fa-trash")) {
+    //? ilgili li elementini silerek DOM'u güncelle
+    e.target.parentElement.remove();
+  }
+
+  //!Event, silme veya ok butonlarından geldi ise
+  if (e.target.classList.contains("fa-check")) {
+    //? ilgili li elementinde checked adinda bir class varsa bunu sil
+    if (e.target.parentElement.classList.contains("checked")) {
+      e.target.parentElement.classList.remove("checked");
+    } else {
+      //? ilgili li elementinde checked adinda bir class yoksa ekle
+      e.target.parentElement.classList.add("checked");
+    }
+  }
+});
